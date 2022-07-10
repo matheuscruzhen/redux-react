eb484d28aae2bf8f562a8a344fcf12d29c07271d
Matheus <matheuscruzhen@gmail.com>
2022-07-10 15:05:49 -0300

    subscribe and dispatch

--- src/components/Bugs.jsx
+++ src/components/Bugs.jsx
@@ -1,17 +1,35 @@
 import React from 'react'
 import { Component } from 'react';
 import StoreContext from '../contexts/storeContext';
+import bugs, {loadBugs} from '../store/bugs';
 
 class Bugs extends Component {
   static contextType = StoreContext;
+
+  state = { bugs: []};
   
   componentDidMount() {
-    console.log(this.context)
+    const store  = this.context;
+    
+    this.unsubscribe = store.subscribe(() => {
+      const bugsInStore = store.getState().entities.bugs.list;  
+      if (this.state.bugs !== bugsInStore) this.setState({ bugs: bugsInStore});
+    })
+
+    store.dispatch(loadBugs());    
+  }
+
+  componentWillMount() {
+    this.unsubscribe;
   }
   
   render() {
   return (
-    <div>Bugs</div>
+    <ul>
+      {
+        this.state.bugs.map(bug => <li key={bug.id}>{bug.description}</li>)
+      }
+    </ul>
     )
   }
 }
